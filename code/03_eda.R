7251778f754020ccc5bb2bdfacaa2f7191a8727a
z7 <55432926+AngZiqi@users.noreply.github.com>
2026-07-11 23:12:05 +0200

    some corrections

--- code/03_eda.R
+++ code/03_eda.R
@@ -11,19 +11,14 @@ data <- read_csv(here("data", "processed", "data_clean.csv"))
 glimpse(data)
 
 
-# Multi-lingual interactive analysis with Python & R (Ziqi)
+# Multi-lingual interactive analysis with Python & R 
 install.packages("reticulate") #set up embedded python environment 
 library(reticulate)
 
 # import python libraries
-
-
 # 1. Overall seasonality with cohort map
-# 2. PCA to reduce dimensions especially with multi-collinear variables
-# 3. prepare time-series data
-# 3.0 check the year and month completeness
-# 3.1 isolate outliers, smooth out noises
-# 3.2 STL decomposition for seasonality, cynical patterns
-# 4. validate initial assumptions
+# 2. STL decomposition for seasonality, cynical patterns
+# 3. validate initial assumptions
 # !make plots interactive, ggplotly or gganimate
+
 
