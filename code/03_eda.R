9faa175d9594e32fa5cca2cedfcf54e4faa03d9f
z7 <55432926+AngZiqi@users.noreply.github.com>
2026-07-11 15:11:18 +0200

    add concrete analysis steps

--- code/03_eda.R
+++ code/03_eda.R
@@ -1,5 +1,5 @@
 # 03_eda.R
-# Exploratory data analysis: distributions, missingness, relationships.
+# Exploratory data analysis: distributions, missingness, relationships, discover hidden patterns, understand baselines, check assumptions
 # Figures are saved to docs/ for inclusion in report.qmd.
 
 library(tidyverse)
@@ -9,3 +9,21 @@ data <- read_csv(here("data", "processed", "data_clean.csv"))
 
 # TODO: add EDA plots and summaries
 glimpse(data)
+
+
+# Multi-lingual interactive analysis with Python & R (Ziqi)
+install.packages("reticulate") #set up embedded python environment 
+library(reticulate)
+
+# import python libraries
+
+
+# 1. Overall seasonality with cohort map
+# 2. PCA to reduce dimensions especially with multi-collinear variables
+# 3. prepare time-series data
+# 3.0 check the year and month completeness
+# 3.1 isolate outliers, smooth out noises
+# 3.2 STL decomposition for seasonality, cynical patterns
+# 4. validate initial assumptions
+# !make plots interactive, ggplotly or gganimate
+
